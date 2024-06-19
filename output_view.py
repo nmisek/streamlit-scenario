727764089487128ad2e99310f0b5590b64fbccf5
Nicole Misek <nicole@nextmv.io>
2024-06-18 22:24:29 -0400

    .

--- output_view.py
+++ output_view.py
@@ -12,7 +12,7 @@ st.set_page_config(layout="wide")
 query_params = st.query_params
 
 app_id = query_params.get("app_id")
-run_id = query_params.get("run_id")
+batch_id = query_params.get("batch_id")
 api_base_url = unquote(query_params.get("url", ""))
 
 init_auth_state()
@@ -22,8 +22,8 @@ error = False
 if app_id is None or app_id == "":
     app_id = "temp-demand-forecast"
 
-if run_id is None or run_id == "":
-    run_id = "devint-BGS39HySR"
+if batch_id is None or batch_id == "":
+    batch_id = "sdfsdfs"
 
 if api_base_url == "" or api_base_url is None:
     api_base_url = "https://api.cloud.nextmv.io"
@@ -31,32 +31,12 @@ if api_base_url == "" or api_base_url is None:
 if error:
     st.stop()
 
-
-# @st.experimental_dialog("Enter your API key")
-# def get_api_key():
-#     api_key = st.text_input("API Key", type="password")
-#     if st.button("Submit"):
-#         st.session_state["api_key"] = api_key
-#         st.rerun()
-
-
-# # set API key secret from .streamlit/secrets.toml
-# # use either secret if it exists or dialog to get API key
-# # check if it's in st.secrets first
-# # initialize st.session_state["api_key"] if it's not there
-# # if it's there, use it
-
-# # get api key from secrets
-# api_key = st.secrets["NEXTMV_API_KEY"]
-# if api_key is None or api_key == "":
-#     get_api_key()
-
-
 headers = st.session_state.headers
 
-runs_url = f"{api_base_url}/v1/applications/{app_id}/runs/{run_id}"
+results_url = f"{api_base_url}/v1/applications/{app_id}/experiments/batch/{batch_id}"
+response = req.get(results_url, headers=headers)
 
-response = req.get(runs_url, headers=headers)
+st.write(response.json)
 
 if (
     response.status_code == 403 or response.status_code == 401
