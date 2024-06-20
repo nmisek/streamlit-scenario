e2f256cd48baf0b2c2cfc4e2c47a20088d9a9972
Nicole Misek <nicole@nextmv.io>
2024-06-19 21:17:08 -0400

    .

--- output_view.py
+++ output_view.py
@@ -1,4 +1,3 @@
-import math
 from urllib.parse import unquote
 
 import altair as alt
@@ -119,12 +118,8 @@ selected_column = st.selectbox("Select a statistic:", columns)
 df_filtered = df[df["indicator"] == selected_indicator]
 if selected_column == "mean":
     st.write(df_filtered)
-    df_filtered["lower_bound"] = df_filtered["mean"] - 1.96 * df_filtered[
-        "std"
-    ] / math.sqrt(df_filtered["count"])
-    df_filtered["upper_bound"] = df_filtered["mean"] + 1.96 * df_filtered[
-        "std"
-    ] / math.sqrt(df_filtered["count"])
+    df_filtered["lower_bound"] = df_filtered["mean"] - 1.96 * df_filtered["std"]
+    df_filtered["upper_bound"] = df_filtered["mean"] + 1.96 * df_filtered["std"]
     st.write(df_filtered)
 
 if selected_column == "mean":
